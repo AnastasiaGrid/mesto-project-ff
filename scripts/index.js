e181bce467dac7268d1b9a26dda5568b02df3acb
Anastasia Gridneva <anastasia.gridneva@mail.ru>
2024-04-02 09:47:47 +0300

    Fixed code based on reviewer's comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,29 @@
 // @todo: DOM узлы
-const placesList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content
 const cardElement = cardTemplate.querySelector('.places__item')
 
 // @todo: Функция создания карточки
-function addCard(item, deleteCard) {
+function createCard(cardData, deleteCard) {
   const cloneCard = cardElement.cloneNode(true);
-  cloneCard.querySelector('.card__title').textContent = item.name;
-  cloneCard.querySelector('.card__image').src = item.link;
+  cloneCard.querySelector('.card__title').textContent = cardData.name;
+  cloneCard.querySelector('.card__image').src = cardData.link;
+  cloneCard.querySelector('.card__image').alt = cardData.name;
 
   const deleteButtonCard = cloneCard.querySelector('.card__delete-button');
-  deleteButtonCard.addEventListener('click', deleteCard);
+  deleteButtonCard.addEventListener('click', () => deleteCard(cloneCard));
   return cloneCard
 }
 
 // @todo: Функция удаления карточки
-function deleteCard(event) {
-  const target = event.target.closest('.places__item')
-  target.remove()
+function deleteCard(card) {
+  card.remove()
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function (item) {
-  placesList.append(addCard(item,deleteCard))
+initialCards.forEach(function (cardData) {
+  cardsContainer.append(createCard(cardData,deleteCard))
 })
 
